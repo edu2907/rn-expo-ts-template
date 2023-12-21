4ac1f6cce8949d563c075817ed00dab02b1d4dfd
eduardo <eduardo.carneiro2907@gmail.com>
2023-12-20 21:45:22 -0300

    Setup linting and format

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: '18.2.0',
+    },
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'linebreak-style': ['error', 'unix'],
+  },
+}
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+const favFruits: string[] = ['apple', 'banana', 'orange']
+
+function addFruit(fruit: string) {
+  favFruits.push(fruit)
+}
+
+addFruit('grape')
